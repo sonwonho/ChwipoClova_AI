3d1262c83274144c816e451510a5bdafc8eaa189
sonwonho <sonwonho2005@gmail.com>
2023-12-13 22:16:21 +0900

    Add LLM api

--- tikitaka.py
+++ tikitaka.py
@@ -3,11 +3,13 @@ import os
 
 from flask import Flask, Response, request, send_file
 
+from utils.calctoken import TokenCalculator
 from utils.clovaocr import FILE_OCR
 from utils.llm import LLM
 
 app = Flask(__name__)
 fo = FILE_OCR()
+tc = TokenCalculator()
 llm = LLM()
 
 @app.route('/file_test', methods=["POST"])
@@ -31,12 +33,35 @@ def ocr_file():
     text = fo.byte_convert_txt(file)
     return Response(bytes(text, 'utf-8'), mimetype="text/plain")
 
+@app.route('/count_token', methods=["POST"])
+def count_token():
+    btext = request.data
+    text = str(btext, 'utf-8')
+    token_count = tc.calculate(text)
+    return Response(token_count, mimetype="text/plain")
+
 @app.route('/llm/resume', methods=["POST"])
 def llm_resume():
     btext = request.data
     text = str(btext, 'utf-8')
-    result_json = llm.resume_summary(text)
-    return Response(result_json, mimetype="application/json")
+    result_response = llm.resume_summary(text)
+    return Response(result_response, mimetype="application/json")
+
+@app.route('/llm/recruit', methods=["POST"])
+def llm_recruit():
+    btext = request.data
+    text = str(btext, 'utf-8')
+    result_response = llm.recruit_summary(text)
+    return Response(result_response, mimetype="application/json")
+
+@app.route('/llm/question', methods=["POST"])
+def llm_question():
+    input_json = json.loads(request.json)
+    recruit_summay = str(input_json["recruit_summay"])
+    resume_summary = str(input_json["resume_summary"])
+    result_response = llm.make_question(recruit_summay, resume_summary)
+    return Response(result_response, mimetype="application/json")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
