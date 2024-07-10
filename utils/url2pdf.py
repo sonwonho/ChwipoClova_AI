7515d5e72d7eb8812a4cbf77f463ae2d81380aa7
Ho <sonwonho2005@gmail.com>
2024-07-10 09:44:00 +0900

    Fix: 403 Forbidden

--- utils/url2pdf.py
+++ utils/url2pdf.py
@@ -6,7 +6,10 @@ from playwright.async_api import async_playwright
 async def get_pdf(url):
     async with async_playwright() as p:
         browser = await p.chromium.launch()
-        page = await browser.new_page()
+        context = await browser.new_context(
+            user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/109.0"
+        )
+        page = await context.new_page()
         await page.goto(url)
         await page.emulate_media(media="screen")
         pdf_bytes = await page.pdf(prefer_css_page_size=True)
@@ -14,9 +17,14 @@ async def get_pdf(url):
         return pdf_bytes
 
 
+async def url_to_pdf(url, output_path):
+    pdf_data = await get_pdf(url)
+    with open(output_path, "bw") as f:
+        f.write(pdf_data)
+
+
 if __name__ == "__main__":
     # Example usage
-    url = "https://www.naver.com"
-    # output_path = "html-to-pdf-output.pdf"
-    b = asyncio.run(get_pdf(url))
-    print(b)
+    url = "https://www.wanted.co.kr/wd/225097"
+    output_path = "html-to-pdf-output.pdf"
+    asyncio.run(url_to_pdf(url, output_path))
